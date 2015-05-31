80286b520ee63b800456a10da52e84000778dea2
Ben Frisby <bfrisb@gmail.com>
2015-05-31 20:00:02 +0100

    not sure what or where I am at commited most my work out

--- public/script.js
+++ public/script.js
@@ -1,27 +1,40 @@
-(document).ready(function(){
-
-
 function getVideos(){
+  console.log('hope')
   //Ajax request to retrive all the items from the database
   $.ajax({
     type: "GET",
     url: "/videos",
     dataType: 'json'
-  }).done(function)(data){
+  }).done(function(data){
     $.each(data, function(index, item){
+      debugger;
+      binding.pry
       appendNewItem(item);
     })
-  }
-}
-
-function uploadVideo(){
-  $.ajax({
-    type: 'POST',
-    url: '/items',
-    dataType: 'json', 
-  }).done(function(data){
-
   })
 }
+ function uploadVideo(){
+  
+console.log('help')
+ }
 
-})
+//   var addNew = $('#stat')
+
+//   console.log(addNew)
+//   debugger;
+//   binding.pry
+//   $.ajax({
+//     type: 'POST',
+//     url: '/items',
+//     dataType: 'json', 
+//   }).done(function(data){
+//     console.log("help")
+//     debugger;
+//     nil
+//   })
+
+
+// Define Variables
+var uploadVid = $('#stat');
+// //create event listener's
+uploadVid.on('click', uploadVideo);
